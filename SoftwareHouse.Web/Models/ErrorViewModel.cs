75898e2afb912525129156ee5a651f736cd3274f
darekmaciag@wp.pl <darekmaciag@wp.pl>
2018-04-22 19:25:52 +0200

    Add project files.

---
+++ SoftwareHouse.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SoftwareHouse.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
